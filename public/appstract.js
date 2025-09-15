ae9b7b364354cb8e52d73ebe7d7467b055df9b7f
Alexander Auermaa <alexander.auermaa@gmail.com>
2025-09-15 22:00:58 +0200

    script fixes

--- public/appstract.js
+++ public/appstract.js
@@ -1,19 +1,26 @@
-const el = document.getElementsByTagName("appstract-form");
+let iframe = null;
 
-if (el && el.length > 0) {
-  this.initIframe(el[0]);
+function initializeIframe(el) {
+  iframe = document.createElement("iframe");
+  iframe.src = "https://karlsfors.appstract.se/";
+  iframe.style.border = "0";
+  iframe.style.width = "100%";
+  iframe.height = "0";
+  el.appendChild(iframe);
 }
-window.onmessage = (e) => {
+
+const el = document.getElementsByTagName("appstract-form");
+if (el && el.length > 0) {
+  initializeIframe(el[0]);
+}
+
+window.addEventListener("message", (e) => {
   const data = e.data;
   if (data && data.appstract) {
     const msg = data.message;
-    if (msg.type == "height") {
-      if (this.iframe) {
-        this.iframe.height = Math.max(msg.data, 400).toString();
-        console.log("[Appstract] height set to: ", this.iframe.height);
-      }
+    if (msg.type === "height" && iframe) {
+      iframe.height = Math.max(msg.data, 0).toString();
+      // console.log("[Appstract] height set to:", iframe.height);
     }
-
-    return;
   }
-};
+});
